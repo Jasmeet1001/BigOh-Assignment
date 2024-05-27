93fd8bb7af229e173e23d22acfaf5a2814e3393c
Jasmeet <js3393187@gmail.com>
2024-05-27 17:44:53 +0530

    Added run script and improved error handling,logs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routes/routes.js";
+import createError from "http-errors";
+import logger from "morgan";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-  return res.status(200).json({message: "Welcome to BigOh API"})
+  return res.status(200).json({ message: "Welcome to BigOh API" });
 });
 
 app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.use(function (req, res, next) {
+  next(createError(404));
 });
+
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
